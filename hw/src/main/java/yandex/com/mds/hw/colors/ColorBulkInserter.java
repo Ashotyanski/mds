20273e4ce4ddd0308d009b381f65d759d780ef7a
Ashotyanski <ashotk0510@gmail.com>
2017-05-10 18:24:47 +0500

    Fixes and refactoring

--- hw/src/main/java/yandex/com/mds/hw/colors/ColorBulkInserter.java
+++ hw/src/main/java/yandex/com/mds/hw/colors/ColorBulkInserter.java
@@ -27,44 +27,51 @@ import static yandex.com.mds.hw.db.ColorDatabaseHelper.ColorEntry.TITLE;
 
 class ColorBulkInserter {
     private static final String TAG = ColorBulkInserter.class.getName();
-    private static final int TOTAL_COUNT = 100000;
-    private static final int TRANSACTION_SIZE = 200;
     private static int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
     private static final int KEEP_ALIVE_TIME = 1;
     private static final TimeUnit KEEP_ALIVE_TIME_UNIT = TimeUnit.SECONDS;
+
+    private int totalCount = 100000;
+    private int transactionSize = 200;
 
     private Context context;
     private ProgressDialog progress;
     private ColorDatabaseHelper databaseHelper;
     private ExecutorService service;
     private Handler mHandler;
-
     private OnInsertFinishListener listener;
 
-    public ColorBulkInserter(Context context, OnInsertFinishListener listener) {
+    private static ColorBulkInserter instance;
+
+    public static ColorBulkInserter getInstance(Context context) {
+        if (instance == null)
+            instance = new ColorBulkInserter(context);
+        return instance;
+    }
+
+    private ColorBulkInserter(Context context) {
         this.context = context;
-        this.listener = listener;
         databaseHelper = ColorDatabaseHelper.getInstance(context);
         BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
         service = new ThreadPoolExecutor(NUMBER_OF_CORES, NUMBER_OF_CORES, KEEP_ALIVE_TIME, KEEP_ALIVE_TIME_UNIT, queue);
-
         mHandler = new Handler(Looper.myLooper()) {
-            int i = TOTAL_COUNT / TRANSACTION_SIZE;
+            int i = totalCount / transactionSize;
             final Object lock = new Object();
 
             @Override
             public void handleMessage(Message msg) {
                 super.handleMessage(msg);
                 if (msg.what == 2) {
-                    progress.incrementProgressBy(TRANSACTION_SIZE);
+                    progress.incrementProgressBy(transactionSize);
                     synchronized (lock) {
                         i--;
                     }
                     if (i == 0) {
-                        Log.d(TAG, "Bulk inserted " + TOTAL_COUNT + " colors");
+                        Log.d(TAG, "Bulk inserted " + totalCount + " colors");
                         progress.dismiss();
-                        ColorBulkInserter.this.listener.onFinishInsert();
-                        i = TOTAL_COUNT / TRANSACTION_SIZE;
+                        if (listener != null)
+                            listener.onFinishInsert();
+                        i = totalCount / transactionSize;
                     }
                 }
             }
@@ -75,14 +82,16 @@ class ColorBulkInserter {
         progress = new ProgressDialog(context);
         progress.setMessage("Inserting colors");
         progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progress.setMax(TOTAL_COUNT);
+        progress.setMax(totalCount);
         progress.show();
 
-        for (int i = 0; i <= TOTAL_COUNT; i += TRANSACTION_SIZE)
-            service.execute(new BulkInsertRunnable(i, i + TRANSACTION_SIZE));
+        for (int i = 0; i <= totalCount; i += transactionSize)
+            service.execute(new BulkInsertRunnable(i, i + transactionSize));
     }
 
+
     private class BulkInsertRunnable implements Runnable {
+
         int from, to;
 
         public BulkInsertRunnable(int from, int to) {
@@ -95,8 +104,10 @@ class ColorBulkInserter {
             SQLiteDatabase db = databaseHelper.getWritableDatabase();
             db.beginTransaction();
             try {
+                ContentValues contentValues;
                 for (int i = from; i <= to; i++) {
-                    ContentValues contentValues = generateColorRecord(i);
+                    contentValues = new ContentValues();
+                    generateColorRecord(contentValues, i);
                     db.insert(TABLE_NAME, null, contentValues);
                 }
                 db.setTransactionSuccessful();
@@ -110,16 +121,27 @@ class ColorBulkInserter {
             Message completeMessage = mHandler.obtainMessage(2);
             completeMessage.sendToTarget();
         }
+
     }
 
-    private ContentValues generateColorRecord(int seed) {
-        ContentValues contentValues = new ContentValues();
+    private void generateColorRecord(ContentValues contentValues, int seed) {
         contentValues.put(COLOR, seed);
         contentValues.put(TITLE, String.valueOf(seed));
         contentValues.put(DESCRIPTION, "This is for " + String.valueOf(seed));
         contentValues.put(CREATION_DATE, new Date().getTime());
         contentValues.put(IMAGE_URL, "https://yandex.ru/search/xml?&key=qwerty&query=" + String.valueOf(seed));
-        return contentValues;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public void setTransactionSize(int transactionSize) {
+        this.transactionSize = transactionSize;
+    }
+
+    public void setOnInsertFinishListener(OnInsertFinishListener listener) {
+        this.listener = listener;
     }
 
     public interface OnInsertFinishListener {
