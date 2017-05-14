fda922c26f99ee2efd692eca61363d41bd775c3a
Ashotyanski <ashotk0510@gmail.com>
2017-05-14 23:19:15 +0500

    Redundant code removed

--- hw/src/main/java/yandex/com/mds/hw/colors/ColorsActivity.java
+++ hw/src/main/java/yandex/com/mds/hw/colors/ColorsActivity.java
@@ -2,9 +2,6 @@ package yandex.com.mds.hw.colors;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -74,32 +71,6 @@ public class ColorsActivity extends AppCompatActivity implements QueryPresenter.
         startActivityForResult(ColorEditActivity.getInstance(this, colorId), COLOR_EDIT_REQUEST_CODE);
     }
 
-    private void playSound(double frequency, int duration) {
-        // AudioTrack definition
-        int mBufferSize = AudioTrack.getMinBufferSize(44100,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_8BIT);
-
-        AudioTrack mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-                AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT,
-                mBufferSize, AudioTrack.MODE_STREAM);
-
-        // Sine wave
-        double[] mSound = new double[4410];
-        short[] mBuffer = new short[duration];
-        for (int i = 0; i < mSound.length; i++) {
-            mSound[i] = Math.sin((2.0 * Math.PI * i / (44100 / frequency)));
-            mBuffer[i] = (short) (mSound[i] * Short.MAX_VALUE);
-        }
-
-        mAudioTrack.setStereoVolume(AudioTrack.getMaxVolume(), AudioTrack.getMaxVolume());
-        mAudioTrack.play();
-
-        mAudioTrack.write(mBuffer, 0, mSound.length);
-        mAudioTrack.stop();
-        mAudioTrack.release();
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -130,7 +101,6 @@ public class ColorsActivity extends AppCompatActivity implements QueryPresenter.
         switch (item.getItemId()) {
             case R.id.action_filter: {
                 presenter.toggleQuery();
-                playSound(440, 44100);
                 return true;
             }
             case R.id.action_import_export: {
